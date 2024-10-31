9fbbd28ebf9be9f3f4efaba511fe55d80ace625b
Rajchal <rajchalanjal1@gmail.com>
2024-11-01 00:33:44 +0545

    Tailwind Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 const Home = () => {
-  return <h1 className="text-3xl font-black text-white">Welcome to NextJs</h1>;
+  return (
+    <h1 className="text-light-500 text-3xl font-black">Welcome to NextJs</h1>
+  );
 };
 export default Home;
