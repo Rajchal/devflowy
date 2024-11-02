d10ba1f06dc56b699d530ad6c0ea45bdebdeb73d
Rajchal <rajchalanjal1@gmail.com>
2024-11-02 15:51:52 +0545

    Authentication using Github

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,28 +1,21 @@
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
-const Home = () => {
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
   return (
     <>
-      <h1 className="h1-bold">Welcome</h1>
-      <h1 className="h1-bold font-inter">Welcome</h1>
-      <DropdownMenu>
-        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuItem>Subscription</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log Out</Button>
+      </form>
     </>
   );
 };
