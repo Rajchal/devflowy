636061799ffe988fffd5a7d104e99b4c5e3eca52
Rajchal <rajchalanjal1@gmail.com>
2024-11-05 00:42:14 +0545

    Home page layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,24 @@
-import { auth } from "@/auth";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
 const Home = async () => {
-  const session = await auth();
-  console.log(session);
-  return <></>;
+  return (
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">Local Search</section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6"></div>
+    </>
+  );
 };
 export default Home;
